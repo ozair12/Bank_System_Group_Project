af41a2e03031164fc888f03a35d6bda24a935006
Mahbub khan <mahbubk@live.com>
2022-06-21 13:05:37 +0100

    Commit after adding biz account charge and isa account interest methods

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
     private HashMap<LocalDateTime,Double> statement;
     public static void main(String[] args) {
         PersonalAccount pc=new PersonalAccount("Jon Smith",010506,87654321,1000.00,  LocalDateTime.now());
-       // BusinessAccount ba=new BusinessAccount("Jon Smith",090203,72945623,1000.0,LocalDateTime.now());
+        BusinessAccount ba=new BusinessAccount("Jon Smith",323536,78965322,1000.0,LocalDateTime.now());
         ISAAccount ia=new ISAAccount("John Smith",070302,73498251,1050.0,LocalDateTime.now());
         pc.setStatement(pc.openingTime,pc.balance);
         System.out.println(pc.getName());
@@ -22,6 +22,8 @@ public class App {
         System.out.println("Printing Account Statement for "+pc.getName() +"\n  "  + pc.getStatement());
         //ba.addAccCharge(25.0);
         //System.out.println("Your Current Charges "+ba.getCharges());
+        System.out.println("Printing Business Account Statement for "+ba.getName() +"\n  "  + ba.getStatement());
+        System.out.println("ISA Account Statement for "+ia.getName() +"\n  "  + ia.getStatement());
 
 
     }
